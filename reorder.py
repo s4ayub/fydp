ae1cb2d06edf06caa6ab948d3efa010a63ddbdf0
HarminderLall <h.h32107988@hotmail.com>
2020-02-03 15:29:42 -0500

    Create reorder.py
    Reordering csv's to be compatible with audio_splitter.py script

---
+++ reorder.py
@@ -0,0 +1,8 @@
+import csv
+
+# Creating script to tranform the transcripts to appropriate form for other paper 
+# TODO:
+# 1. Query all files with .csv endings 
+# 2. Read the files line by line, lump preceeding stutters into one, ignore stutters with label 'i' [removing interjection]
+# 3. Write to a .txt file in the format 'label start_time end_time', 
+#    name of text file should be the same as the cvs maybe relocate it  
