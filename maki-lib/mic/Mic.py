836aab70ac72a13f6c9aef78a753f166a7938b4c
Arnet Tran <arnettran@gmail.com>
2020-02-28 21:18:43 -0500

    Maki Mic Lib Update: int message passing

--- maki-lib/mic/Mic.py
+++ maki-lib/mic/Mic.py
@@ -1,49 +1,123 @@
 from MicIO import MicIO
 import socket
+import select
 import threading
 import numpy as np
+import time
+import sys
+
+AudioTimeout = 0.5
+
+SyncMessageSize = sys.getsizeof(int())
+SyncTimeout = 2
 
 class MicTransmitter:
     def __init__(self):
         self.micIO = MicIO()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.audio_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sync_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def callback(self, data):
-        self.socket.sendall(data)
+        self.sync_socket.settimeout(SyncTimeout)
 
-    def start(self, host, port, device_index=None):
+    def connect(self, host, port1, port2):
+        print('MicTransmitter: Connecting to %s on ports %d,%d ...' % (host, port1, port2))
+
         try:
-            self.socket.connect((host, port))
+            self.audio_socket.connect((host,port1))
+            self.sync_socket.connect((host, port2))
         except:
+            print('MicTransmitter: Connection refused')
             return False
-        self.micIO.listen(self.callback, device_index)
+
         return True
 
+    def disconnect(self):
+        self.audio_socket.close()
+        self.sync_socket.close()
+
+    def audio_callback(self, data):
+        self.audio_socket.sendall(data)
+
+    def start(self, device_index=None):
+        self.micIO.listen(self.audio_callback, device_index)
+
     def stop(self):
-        self.socket.close()
         self.micIO.stop()
+
+    def sync_wait(self):
+        msg = 0
+        try:
+            msg = self.sync_clientsocket.recv(SyncMessageSize)
+            msg = int.from_bytes(msg, 'little')
+        except socket.timeout:
+            print('MicReceiever: Sync error timeout')
+
+        return msg
+
+    def sync_post(self, msg=0):
+        msg = bytes([int(msg)])
+        self.sync_socket.sendall(msg)
 
 class MicReceiver:
     def __init__(self, callback):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.callback = callback
 
-    def listen(self, host, port):
-        self.socket.bind((host, port))
-        self.socket.listen(1)
+        self.audio_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sync_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        (clientsocket, address) = self.socket.accept()
-        print('Mic connection from %s,%d' % address)
+        self.audio_clientsocket = None
+        self.sync_clientsocket = None
 
+    def connect(self, host, port1, port2):
+        self.audio_socket.bind((host, port1))
+        self.sync_socket.bind((host, port2))
+
+        self.audio_socket.listen(1)
+        self.sync_socket.listen(1)
+
+        print('MicReceiver: Waiting for connections to %s on ports %d,%d ...' % (host, port1, port2))
+
+        (self.audio_clientsocket, _) = self.audio_socket.accept()
+        (self.sync_clientsocket, _) = self.sync_socket.accept()
+
+        self.audio_clientsocket.settimeout(AudioTimeout)
+        self.sync_clientsocket.settimeout(SyncTimeout)
+
+        print('MicReceiver: Mic connection established')
+
+    def disconnect(self):
+        self.audio_socket.close()
+        self.sync_socket.close()
+
+    def listen(self):
         while True:
-            data = clientsocket.recv(MicIO.FramesPerBuffer)
-            if not data:
-                print('Connection was closed.')
+            try:
+                data = self.audio_clientsocket.recv(MicIO.FramesPerBuffer)
+                if not data:
+                    break
+            except:
                 break
 
             data = np.frombuffer(data, dtype=np.int16).tolist()
             if self.callback:
                 self.callback(data)
         
-        self.socket.close()
+    def sync_wait(self):
+        msg = 0
+        r, _, _ = select.select([self.sync_clientsocket], [], [], SyncTimeout)
+
+        if r:
+            try:
+                msg = self.sync_clientsocket.recv(SyncMessageSize)
+                msg = int.from_bytes(msg, 'little')
+            except:
+                pass
+        else:
+            print('MicReceiever: Sync error timeout')
+
+        return msg
+
+    def sync_post(self, msg=0):
+        msg = bytes([int(msg)])
+        self.sync_clientsocket.sendall(msg)
 
--- maki-lib/mic/MicTest.py
+++ maki-lib/mic/MicTest.py
@@ -1,21 +1,37 @@
 import Mic
 import sys
+import time
 
 HOST='localhost'
-PORT=2000
+PORT1=2000
+PORT2=2001
 
 def received_data(data):
     pass
 
-if sys.argv[1] == 'rx':
+mode = sys.argv[1]
+
+if mode == 'rx':
     rx = Mic.MicReceiver(received_data)
-    rx.listen(HOST, PORT)
-elif sys.argv[1] == 'tx':
+    rx.connect(HOST, PORT1, PORT2)
+
+    rx.listen()
+    msg = rx.sync_wait()
+    print('Transmission was stopped. Received msg=%d.' % msg)
+
+    rx.disconnect()
+
+if mode == 'tx':
+    msg = 3
     tx = Mic.MicTransmitter()
-    if tx.start(HOST, PORT):
+    success = tx.connect(HOST, PORT1, PORT2)
+
+    if success:
+        tx.start()
         input('Press any key to stop.')
-        print('Stopping connection.')
         tx.stop()
-    else:
-        print('Connection refused.')
+
+        print('Stopping transmission with msg=%d.' % msg)
+        tx.sync_post(msg)
+        tx.disconnect()
 
